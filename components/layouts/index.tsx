f8f592aa594010eb52a33ea430aaebc930ca663e
Jenny <jenny.maya186@gmail.com>
2023-10-19 17:57:40 +0700

    Updated the Layout File

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import Header from "../landing/Header";
-import Footer from "../landing/Footer";
+import React, { ReactNode } from 'react';
 
-const MainLayout: any = ({ children }) => {
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <>
-      <div className=" bg-orange-50  h-screen">
-        <Header />
-        <div className="scrollable-container h-[90vh]">{children}</div>
-        <Footer />
-      </div>
+      <main>{children}</main>
     </>
   );
-};
+}
 
 export default MainLayout;
