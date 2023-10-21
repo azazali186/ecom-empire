cf30cb06b1232166c2b5048223430da9a3267f5d
Jenny <jenny.maya186@gmail.com>
2023-10-21 18:00:21 +0700

    Updated

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -2,7 +2,6 @@ import SideBanner from "@/components/landing/banner/SideBanner";
 import MainLayout from "@/components/layouts";
 import { Product } from "@/components/product/ProductCard";
 import { useMediaQuery } from "@mui/material";
-import { useRouter } from "next/router";
 import React from "react";
 type ProductDetailsProps = {
   data: {
@@ -17,14 +16,27 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ data }) => {
     <MainLayout>
       <div className="flex mx-20 mt-20 flex-col">
         <div className="flex">
-          <div className="w-[25%]  bg-orange-100">
+          <div className="w-[25%] mr-20 bg-orange-100">
             <SideBanner
               images={product.images}
               styles={{ height: "50vh", width: isSmallScreen ? "100%" : "100%" }}
               sliderStyles={{ height: "100%" }}
             />
           </div>
-          <div>Item Details</div>
+          <div className="">
+              <div className="font-bold text-4xl text-orange-400">
+                {product.title}
+              </div>
+              <div className="mt-5 text-xl font-bold text-orange-400">
+                Stock : {product.stock}
+              </div>
+              <div className="mt-2 text-xl font-bold text-orange-400">
+                Price : ${product.price?.toFixed(2)}
+              </div>
+              <div className="mt-2 capitalize text-xl font-bold text-orange-400">
+                Category : {product.category}
+              </div>
+          </div>
         </div>
         <div className="flex flex-col mt-10 ">
           <div>{product.description}</div>
